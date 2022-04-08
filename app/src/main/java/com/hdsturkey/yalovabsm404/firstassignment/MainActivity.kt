56fe1d24c650a519dcd7f7cc56e36d0d5fae5126
Cengiz TORU <cengiztoru@gmail.com>
2022-04-08 12:02:08 +0300

    Sipariş ekranı hazırlandı

--- app/src/main/java/com/hdsturkey/yalovabsm404/firstassignment/MainActivity.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/firstassignment/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hdsturkey.yalovabsm404.firstassignment
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_signup)
+        button.setOnClickListener {
+            startActivity(Intent(this, OrderActivity::class.java))
+        }
+
     }
 }
