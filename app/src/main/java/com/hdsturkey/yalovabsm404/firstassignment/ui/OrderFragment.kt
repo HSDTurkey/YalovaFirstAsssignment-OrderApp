f162d100923edfbaca2788435fa9c36e339659d8
Cengiz TORU <cengiztoru@gmail.com>
2022-05-24 11:40:53 +0300

    Kayıt olma ekranından gelen kullanıcı adı sipariş ekranında gösterildi

--- app/src/main/java/com/hdsturkey/yalovabsm404/firstassignment/ui/OrderFragment.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/firstassignment/ui/OrderFragment.kt
@@ -5,12 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.hdsturkey.yalovabsm404.firstassignment.databinding.FragmentOrderBinding
 
 
 class OrderFragment : Fragment() {
     private lateinit var mBinding: FragmentOrderBinding
+    private val args: OrderFragmentArgs by navArgs()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeViews()
+
+    }
+
+    private fun initializeViews() {
+        setGreetingMessage()
+    }
+
+    private fun setGreetingMessage() {
+        val incomeUserNameSurname = args.nameSurname
+        val upperCasedUserNameSurname = incomeUserNameSurname.uppercase()
+        val greetingMessage = "Hoşgeldin $upperCasedUserNameSurname"
+        mBinding.tvWelcome.text = greetingMessage
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
